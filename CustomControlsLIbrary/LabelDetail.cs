c6f07313f45298f72a3eedd2dd1ee811e51bb655
Lê Yên <140315513+leyen1403@users.noreply.github.com>
2024-09-12 18:38:10 +0700

    add CustomControl Library

---
+++ CustomControlsLIbrary/LabelDetail.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace CustomControlsLIbrary
+{
+    public class LabelDetail:Label
+    {
+        public LabelDetail()
+        {
+            this.Font = new Font("Times New Roman", 13);
+        }
+    }
+}
